5505eb211f93258af73c62dc329bcfe1630770f6
Gabriel Rodriguez <gabriel98rl@gmail.com>
2026-03-23 01:07:09 -0300

    Remove DummyDataSeeder from DatabaseSeeder to streamline seeding process.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CurrencySeeder::class,
             CategorySeeder::class,
-            DummyDataSeeder::class,
         ]);
     }
 }
